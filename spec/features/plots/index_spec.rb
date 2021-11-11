7b3b57f54bd118f7ef67b8615bdce6aa0757cb28
Philip Medlin <phmedlin@gmail.com>
2021-11-10 19:37:48 -0700

    Feat/test: Plots index view and test, plot controller

---
+++ spec/features/plots/index_spec.rb
@@ -0,0 +1,29 @@
+require 'rails_helper'
+
+RSpec.describe 'the plots index page' do
+
+  it 'lists the plot numbers and plants' do
+    garden = Garden.create!(name: 'Christmas Garden')
+    plot_1 = garden.plots.create!
+    plot_2 = garden.plots.create!
+    plant_1 = Plant.create!(name: 'Aloe')
+    plant_2 = Plant.create!(name: 'Agave')
+    PlotPlant.create(plot_id: plot_1.id, plant_id: plant_1.id)
+    PlotPlant.create(plot_id: plot_2.id, plant_id: plant_2.id)
+
+    visit '/plots'
+
+    expect(page).to have_content("Plot Number: #{plot_1.id}")
+    expect(page).to have_content("Plot Number: #{plot_2.id}")
+
+    within("#plot-#{plot_1.id}") do
+      expect(page).to have_content("Aloe")
+      expect(page).not_to have_content("Agave")
+    end
+
+    within("#plot-#{plot_2.id}") do
+      expect(page).to have_content("Agave")
+      expect(page).not_to have_content("Aloe")
+    end
+  end
+end
