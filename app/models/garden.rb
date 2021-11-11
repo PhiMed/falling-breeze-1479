28fdda9933adaa0686e12c56b232199dfc14d91a
Philip Medlin <phmedlin@gmail.com>
2021-11-10 20:51:25 -0700

    Final commits

--- app/models/garden.rb
+++ app/models/garden.rb
@@ -2,10 +2,9 @@ class Garden < ApplicationRecord
   has_many :plots
 
   def unique_plants
-    plots.joins(:plants).where('days_to_harvest < 100').group(:name).pluck(:name)
+    plots.joins(:plants)
+         .where('days_to_harvest < 100')
+         .group(:name)
+         .pluck(:name)
   end
-
-
-  # .where(invoice_items: {status: '0'})
-
 end
